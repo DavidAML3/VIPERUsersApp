4530095690476b32d20103c029731738f74de82d
DavidAML3 <David.aml.3@hotmail.com>
2021-09-09 23:49:26 -0500

    added logic to VIPER files and finished APP

--- VIPERUsersApp/Viper/View.swift
+++ VIPERUsersApp/Viper/View.swift
@@ -6,3 +6,84 @@
 //
 
 import Foundation
+import UIKit
+
+// ViewController
+// protocol
+// reference presenter
+
+protocol AnyView {
+    var presenter: AnyPresenter? { get set }
+    
+    func update(with users: [User])
+    func update(with error: String)
+}
+
+class UserViewController: UIViewController, AnyView {
+    var presenter: AnyPresenter?
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.isHidden = true
+        return label
+    }()
+    
+    var users = [User]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(label)
+        view.backgroundColor = .systemTeal
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        label.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        label.center = view.center
+    }
+    
+    func update(with users: [User]) {
+        self.users = users
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+    
+    func update(with error: String) {
+        self.users = []
+        
+        DispatchQueue.main.async {
+            self.tableView.isHidden = true
+            
+            self.label.text = error
+            self.label.isHidden = false
+        }
+    }
+}
+
+extension UserViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = users[indexPath.row].name
+        return cell
+    }
+}
